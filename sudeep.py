a70c2e16fddfcd3641aca4736a8313ae71ab28a6
narayansarawad2003-sketch <narayansarawad2003@gmail.com>
2026-03-27 12:09:16 +0530

    Add files via upload

---
+++ sudeep.py
@@ -0,0 +1,12 @@
+class hero:
+    def __init__(self):
+        self.name="Sudeep"
+        self.age=52
+        self.numOfMovies=32
+    def act(self):
+        print("sudeep is good actor")
+h1=hero()
+print(h1.name)
+print(h1.age)
+print(h1.numOfMovies)
+h1.act()
